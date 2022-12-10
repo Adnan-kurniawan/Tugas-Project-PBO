9ae72d1818bfae4dcd0501be634064dd770e084e
Adnan-kurniawan <102232836+Adnan-kurniawan@users.noreply.github.com>
2022-12-10 20:42:22 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ public class Main {
         Lele ll = new Lele(10.0);
         Nila nl = new Nila(10.0);
        
-        System.out.println("harga lele : "+ll.imt());
-        System.out.println("harga nila : "+nl.imt());
+        System.out.println("harga lele : "+ll.hrg());
+        System.out.println("harga nila : "+nl.hrg());
 
     }
 }
